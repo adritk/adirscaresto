ee3c601a33d31770b0bb85d0489fe8c084739af2
adritk <adirscatk@gmail.com>
2020-05-31 08:31:44 +0700

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,17 +4,64 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Delivery from '../Images/delivery.svg'
 import Burger from '../Images/burger.svg'
+
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
+} from "mdbreact";
+
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+import CafePizza from '../Images/cafepizza.jpg'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import $ from 'jquery'
-import FastfoodIcon from '@material-ui/icons/Fastfood';
-import { data } from '../myData'
+import { data, cafe, order } from '../myData'
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 class Home extends Component {
 
     componentDidMount() {
-        $("button.btnklik").click(function(){
-            $("button.btnklik").css('background-color','');
-            $(this).css('background-color', '#FCD561'); 
-    });
+        $("button.btnklik").click(function () {
+            $("button.btnklik").css('background-color', '');
+            $(this).css('background-color', '#FCD561');
+        });
+    }
+
+    state = {
+        isOpen: false,
+        totalPrice: 170000,
+        jumlah: 1
+    };
+
+    toggleCollapse = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    tambah = () => {
+        this.setState({ jumlah: this.state.jumlah + 1 })
+    }
+
+    kurang = () => {
+        if (this.state.jumlah < 2) {
+            this.setState({
+                jumlah: 1
+            })
+        } else {
+            this.setState({
+                jumlah: this.state.jumlah - 1
+            })
+        }
+    }
+
+    renderTotal = () => {
+        let jumlah = this.state.jumlah
+        let price = this.state.totalPrice
+        let total = 0
+        total += jumlah * price
+        return (
+            total.toLocaleString()
+        )
     }
 
     render() {
@@ -44,19 +91,110 @@ class Home extends Component {
                             {
                                 data.map(val => {
                                     return (
-                                            <button type="submit" className="btnklik">
-                                                <div className="bulet">
-                                                    <img src={val.src} style={{ width: 35 }} />
-                                                </div>
-                                                <span style={{ fontSize: 12 }}>{val.name}</span>
-                                            </button>
-                                     
+                                        <button type="submit" className="btnklik">
+                                            <div className="bulet">
+                                                <img src={val.src} style={{ width: 35 }} />
+                                            </div>
+                                            <span style={{ fontSize: 12 }}>{val.name}</span>
+                                        </button>
+
                                     )
                                 })
                             }
                         </div>
                     </section>
+
+                    <section id="four-section">
+                        <div className="menu-container">
+                            {
+                                cafe.map(val => {
+                                    return (
+                                        <div className="box-menu">
+                                            <img src={val.src} className="card-menu" />
+                                            <div className="textOnCard">{val.distance}<span style={{ fontSize: 10 }}>Min</span></div>
+                                            <p style={{ fontSize: 18, fontWeight: 500, margin: 10 }}>{val.name}</p>
+                                            <div style={{ margin: 5 }}>
+                                                <StarBorderIcon /><span style={{ paddingRight: 10, fontWeight: 400 }}>{val.rating}</span><span style={{ paddingRight: 10, color: '#A3A3A3' }}>{val.location}</span><span style={{ paddingRight: 10, color: '#A3A3A3' }}  >{val.category}</span>
+                                            </div>
+                                        </div>
+
+                                    )
+                                })
+                            }
+
+                        </div>
+                    </section>
                 </div>
+
+                <aside>
+
+                    <MDBNavbar color="#FFF7ED" dark expand="md">
+                        <MDBNavbarToggler onClick={this.toggleCollapse} />
+                        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                            <MDBNavbarNav right>
+                                <MDBNavItem >
+                                    <MDBDropdown basic>
+                                        <MDBDropdownToggle nav>
+                                            <AccountCircleIcon style={{ fontSize: 36, color: 'grey' }} />
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu className="dropdown-default">
+                                            <MDBDropdownItem href="#!">Login</MDBDropdownItem>
+                                            <MDBDropdownItem href="#!">Register</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                </MDBNavItem>
+                                <MDBNavbarBrand style={{ paddingTop: 10, marginRight: '55px' }}>
+                                    <div className="total-item">3</div>
+                                </MDBNavbarBrand>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBNavbar>
+
+                    <div className="box-order">
+                        <p>Menteng Sreet <span style={{ marginLeft: 115 }}>Edit</span></p>
+                        <button className="button-small" disabled><AccessTimeIcon />&nbsp;35 min</button><span>Choose Time</span>
+                    </div>
+
+                    {
+                        order.map(val => {
+                            return (
+                                <div className="list-menu">
+                                    <img className="order-menu" src={val.src} />
+                                    <span style={{ marginLeft: 25, fontWeight: 500 }}>{val.qty}</span>
+                                    <span style={{ marginLeft: 25, fontWeight: 500 }}>{val.name}</span>
+                                    <span style={{ marginLeft: 40, color: '#BBB6AE', fontSize: 12 }}>{val.price}</span>
+                                </div>
+
+                            )
+                        })
+                    }
+
+                    <div className="list-menu">
+                    <button className="button-delivery" disabled><AirportShuttleIcon fontSize="large" /></button>
+                        <span style={{ marginLeft: 25, fontWeight: 500 }} className="delivery">Delivery</span>
+                      
+                        <span style={{ marginLeft: 40, color: '#BBB6AE', fontSize: 12 }}>IDR 0</span>
+                    </div>
+
+                    {/* <div className="list-menu">
+                        <button className="button-delivery" disabled><AirportShuttleIcon fontSize="large" /></button>
+                        <span className="delivery" style={{ marginLeft: 40, fontWeight: 500 }}>Delivery</span>
+                        <span style={{ marginLeft: 100, color: '#BBB6AE', fontSize: 12 }}>IDR 0</span>
+                    </div> */}
+
+                    <div className="total-payment">
+                        <span className="total">Total :<span style={{ marginLeft: 10 }}>IDR {this.renderTotal()}</span></span>
+                        <hr />
+                    </div>
+
+                    <span style={{ marginLeft: 10, fontWeight: 500, fontSize: 14 }}>Persons</span>
+                    <div className="btnChange">
+                        <button onClick={this.kurang} type="button" className="btn btn-outline-grey  btnTambah">-</button>
+                        <span style={{ paddingRight: 10, fontWeight: 500 }}>{this.state.jumlah}</span>
+                        <button onClick={this.tambah} type="button" className="btn btn-outline-grey btn-rounded waves-effect btnTambah">+</button>
+                        <button type="submit" className="button-checkout"><span style={{ marginRight: 50 }}>Checkout</span><ArrowForwardIcon /></button>
+                    </div>
+                </aside>
             </main>
         );
     }
--- src/myData.js
+++ src/myData.js
@@ -8,6 +8,14 @@ import Dessert from '../src/Images/dessert.svg'
 import Thai from '../src/Images/thai.svg'
 import Sushi from '../src/Images/sushi.svg'
 
+import CafePizza from '../src/Images/cafepizza.jpg'
+import CafePancake from '../src/Images/menupancake.jpg'
+import CafeSushi from '../src/Images/menusushi.jpg'
+
+import Spageti from '../src/Images/spageti.jpg'
+import CocaCola from '../src/Images/cocacola.jpg'
+import Sausage from '../src/Images/sausage.jpg'
+
 
 export const data = [
     {   
@@ -49,5 +57,53 @@ export const data = [
         id: 8,
         name: "Sushi",
         src: Sushi
+    }
+]
+
+export const cafe= [
+    {
+        name: "Bagels",
+        src: Spageti,
+        rating: '4.7',
+        location: 'Jakarta',
+        category: 'Pizza',
+        distance: '20-30'
+    },
+    {
+        name: "The Estaminet",
+        src: CafePancake,
+        rating: "4.5",
+        location: 'Jakarta',
+        category: 'Dessert',
+        distance: '30-40'
+    },
+    {
+        name: "The Estaminet",
+        src: CafeSushi,
+        rating: "4.6",
+        location: 'Jakarta',
+        category: 'Sushi',
+        distance: '40-50'
+    }
+]
+
+export const order = [
+    {
+        qty: '1 x',
+        name: 'Pizza Beef',
+        price: 'IDR 90.000',
+        src: CafePizza
+    },
+    {
+        qty: '1 x',
+        name: 'Bratwurst      ',
+        price: 'IDR 60.000',
+        src: Sausage
+    },
+    {
+        qty: '1 x',
+        name: 'Coca Cola',
+        price: 'IDR 20.000',
+        src: CocaCola
     }
 ]
